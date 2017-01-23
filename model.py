817cbe83c43117f90b575924b1c502f7c503e22a
gada1982 <daniel@gattringer.biz>
2017-01-23 21:34:29 +0100

    Initial imports for testing environment

--- model.py
+++ model.py
@@ -1,1 +1,21 @@
-# Here the model will be defined
+# Model for driving a car 
+# Author: gada1982
+# mail: daniel@gattringer.biz
+
+# General imports
+import os
+import numpy as np
+import csv
+import cv2
+
+# Imports for keras
+from keras.models import Sequential, Model
+from keras.layers import Input, Convolution2D, MaxPooling2D, Flatten
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import Adam
+
+def model_gadaNet():
+  # do nothing
+  return 0
+
+test = model_gadaNet()
